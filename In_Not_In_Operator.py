776949028fa256c061fc8e37bcd1a9722d3dd301
Nayem Uddin <nayemuddin2006@gmail.com>
2025-02-09 22:17:05 +0600

    Add files via upload

---
+++ In_Not_In_Operator.py
@@ -0,0 +1,11 @@
+
+subjects = ["C","C++","Java","Python","OS","Android","TOC"]
+
+print("Python" in subjects)
+print("SQL" not in subjects)
+
+print("SQL" in subjects)
+print("Python" not in subjects)
+
+print(subjects * 3)
+print(subjects + ["Swift",26,27,28])
---
+++ List1.py
@@ -0,0 +1,8 @@
+
+subjects = ["C","C++","Python","Android","Os","TOC"]
+
+print(subjects)
+
+print(subjects[0])
+print(subjects[2:])
+print(subjects[-1])
