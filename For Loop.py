a381f62840bd1c3b0939eabbd2babff4ebc457be
Nayem Uddin <nayemuddin2006@gmail.com>
2025-02-19 21:59:06 +0600

    Add files via upload

---
+++ For Loop.py
@@ -0,0 +1,17 @@
+
+number = [10,20,30,40,50]
+print(number)
+#Using For loop
+for x in number:
+    print(x)
+
+
+#Using while loop it's difficult but using for loop it's too easy
+"""
+index = 0
+n = len(number)
+while index<n :
+    print(number[index])
+    index = index + 1
+"""
+
---
+++ Range_Function.py
@@ -0,0 +1,11 @@
+
+number = list(range(10))
+print(number)
+print(number[2])#Print 2 number index
+print(number[3:])#Print 3 number index to last index
+
+number = list(range(5,11))#We told range is 5 to 11 mean print 5,6,7,8,9,10
+print(number)
+
+number = list(range(2,101,2))#Starting point 2 ending point 101 and difference 2 mean increement2
+print(number)
