997dac76504bf4d3d5f4b96a1e3c7ccc8c9710da
Nayem Uddin <nayemuddin2006@gmail.com>
2025-05-06 21:58:49 +0600

    Delete List1.py

--- List1.py
+++
@@ -1,8 +0,0 @@
-
-subjects = ["C","C++","Python","Android","Os","TOC"]
-
-print(subjects)
-
-print(subjects[0])#For print 0 number of this list
-print(subjects[2:])#for print 2 to last number of this list
-print(subjects[-1])#for print last number off this list
