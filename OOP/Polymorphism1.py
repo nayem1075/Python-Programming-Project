591056661f9b45c9a6ff6a83822b5688e8246c1e
Nayem Uddin <nayemuddin2006@gmail.com>
2025-08-10 22:47:50 +0600

    Add files via upload

---
+++ OOP/Polymorphism1.py
@@ -0,0 +1,25 @@
+
+class Dog:
+    def sound(self):
+        return "Bark"
+
+class Cat:
+    def sound(self):
+        return "Meow"
+
+class Cow:
+    def sound(self):
+        return "Moo"
+
+def animal_sound(animal) :
+    print(animal.sound())
+
+dog = Dog()
+cat = Cat()
+cow = Cow()
+
+animal_sound(dog)
+animal_sound(cat)
+animal_sound(cow)
+
+
