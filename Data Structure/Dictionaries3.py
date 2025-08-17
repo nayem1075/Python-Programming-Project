36987fa73075ac6991fe97a175e4efe19c830fe9
Nayem Uddin <nayemuddin2006@gmail.com>
2025-08-17 20:44:27 +0600

    Add files via upload

---
+++ Data Structure/Dictionaries3.py
@@ -0,0 +1,5 @@
+alien={'color' : 'green','points':5}
+
+print(alien)
+print(alien['color'])
+print(alien['points'])
---
+++ Data Structure/Dictionaries4.py
@@ -0,0 +1,6 @@
+alien={'color':'green','points':5}
+print(alien)
+
+alien['position1']=10
+alien['posiition2']=25
+print(alien)
